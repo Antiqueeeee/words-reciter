d690e7a6630e34a5fcdf0bd5ab5e1842343f4d91
Antiqueeeee <antiqueserver4@gmail.com>
2025-01-20 21:48:39 +0800

    调整tts语速

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ NEO4J_URI = os.getenv('NEO4J_URI', "http://localhost:7474")
 NEO4J_USER = os.getenv('NEO4J_USER', "neo4j")
 NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD', "neo4j")
 
-TTS_DEFAULT_PARMS = {"VOICE" : "en-US-SteffanNeural", "RATE": 0, "VOLUME": 50, "PITCH": 0}
+TTS_DEFAULT_PARMS = {"VOICE" : "en-US-SteffanNeural", "RATE": -30, "VOLUME": 50, "PITCH": 0}
